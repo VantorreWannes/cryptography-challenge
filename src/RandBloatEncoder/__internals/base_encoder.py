522bc9edbc7a000b3c9bfb1ffb259a7fd981ce89
VantorreWannes <VantorreWannes@gmail.com>
2024-08-01 14:44:56 +0200

    Implement an abstract base encoder class.
    - Implement abstract BaseEncoder class.
    - Implement EncodeString method for BaseEncoder.
    - Implement DecodeString method for BaseEncoder.
    
    [Issue: #X]

---
+++ src/RandBloatEncoder/__internals/base_encoder.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+class AdvancedEncoder(ABC):
+
+    @abstractmethod
+    def encodeBytes(self, message: bytes) -> bytes:
+        pass
+
+    @abstractmethod
+    def decodeBytes(self, encoded_message: bytes) -> bytes:
+        pass
+
+    def encodeString(self, message: str) -> bytes:
+        message_bytes: bytes = message.encode('utf-8')
+        return self.encodeBytes(message_bytes)
+    
+    def decodeString(self, encoded_message: bytes) -> str:
+        decoded_message: bytes = self.decodeBytes(encoded_message)
+        return decoded_message.decode('utf-8')
+    
+
+if __name__ == '__main__':
+    pass
